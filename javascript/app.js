729587fdc0eba95a52f572a48c353b957d3e2022
MdNobinfd <mdnobin.fdr@gmail.com>
2025-03-01 17:51:05 +0600

    second commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,41 +1,21 @@
+// This code for Random background color changes 
 const thimChange = document.getElementById("thim_change").addEventListener("click", function() {
-        const hexVal = Math.floor(Math.random()  * 0xffffff).toString(16)
-        document.body.style.background = `#${hexVal}`
-
+    const hexVal = Math.floor(Math.random()  * 0xffffff).toString(16)
+    document.body.style.background = `#${hexVal}`
 })
-
-// const curskfj= new Date()
-// console.log(curskfj)
-// const currentDate = document.getElementById("date").innerText = curskfj
-
-// currentDate.rep
-
-
-// const currentDate = new Date();
-
-// // তারিখ, মাস, এবং বছর আলাদা করে পাওয়া
-// const day = currentDate.getDate();      // দিন
-// const month = currentDate.getMonth() + 1; // মাস (জানুয়ারি = 0, তাই 1 যোগ করতে হবে)
-// const year = currentDate.getFullYear();  // বছর
-// const time = currentDate.getTime();  // বছর
-
-// console.log(`আজকের তারিখ: ${day}/${month}/${year} ${time}}`);
-
-// console.log(new Date().toLocaleString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true }));
-
-// const allCart = document.querySelectorAll(".my_cart button")
-// document.getElementById("cart_length").innerText ="0" + allCart.length;
-
-// const one =allCart.addEventListener("click",function(){
-//     const tittle = document.querySelectorAll('h2')
-//     console.log(tittle)
-// })
-
+// This code for Random background color changes 
+// This code for current day, date, month, and year
+const currentDate = new Date();
+const dateConvert = currentDate.toDateString()
+const dayName = dateConvert.split(' ')[0]
+const monthYear = dateConvert.split(' ').slice(1).join(' ')
+document.getElementById('day_name').innerText = dayName + ' ,';
+document.getElementById('year_month').innerText = monthYear;
+// This code for current day, date, month, and year
+// This code for my main click part and moer code here
 const allCart = document.querySelectorAll('.my_cart');
 const taskLengthElement = document.getElementById("cart_length");
-taskLengthElement.innerText = "0" + allCart.length;
-
-
+taskLengthElement.innerText = allCart.length;
 for (const myCart of allCart) {
     const buttons = myCart.querySelectorAll('button');
     const lastButton = myCart.querySelector('button.onetow')
@@ -44,17 +24,16 @@ for (const myCart of allCart) {
             alert("Board Updated Successfully")
             const title = myCart.querySelector('.title').innerHTML;
             const div = document.getElementById("history")
-            const date= new Date()
+            const time = new Date().toLocaleTimeString();
             const titile = document.createElement("h2")
-            titile.innerHTML = `You have Complete The Task ${title} at ${date}`
+            titile.innerHTML = `You have Complete The Task ${title} at ${time}`
             titile.classList.add('p-[10px]', 'bg-secondary', 'text-[#494a4d]', 'text-base', 'mb-7')
             div.appendChild(titile)
             button.disabled = true;
             button.style.backgroundColor = "#cfd7ff";
-
             let currentTaskLength = parseInt(taskLengthElement.innerText);
             currentTaskLength -= 1;
-            taskLengthElement.innerText = "0" + currentTaskLength; 
+            taskLengthElement.innerText = currentTaskLength; 
             const totalTask = document.getElementById("task_total")
             let convartTotalTask = parseInt(totalTask.innerText)
             convartTotalTask += 1
@@ -65,12 +44,17 @@ for (const myCart of allCart) {
         });
     }
 }
-
-
+// This code for my main click part and moer code here
+// This code for clear history
 document.getElementById("clear_history").addEventListener("click", function() {
     const div = document.getElementById("history")
     div.innerHTML = ""
 })
-
+// This code for clear history
+// This code for navigate blog page location
+document.getElementById("new_discover").addEventListener("click", function() {
+    window.location.href = 'blog.html'
+})
+// This code for navigate blog page location
 
 
---
+++ javascript/blog.js
@@ -0,0 +1,5 @@
+// This code for navigate home page
+document.getElementById("back_desk").addEventListener("click", function() {
+    window.location.href = 'index.html'
+})
+// This code for navigate home page
